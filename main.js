93a34c126f47de78ef3c4fcb825d29cdaea167d1
hudaismail84@yahoo.com <hudaismail84@yahoo.com>
2024-10-22 11:17:15 +0300

    add form mail

--- main.js
+++ main.js
@@ -20,3 +20,29 @@ window.addEventListener('scroll',function(){
 scrollBtn.addEventListener('click',()=>{
 	document.documentElement.scrollTop=0;
 })
+
+
+//send mail
+function sendMail(){
+	var params = {
+		name:document.getElementById("name").value,
+		email:document.getElementById("email").value,
+		message:document.getElementById("message").value,
+	};
+	
+	const serviceID = "service_v43ygrk";
+	const templateID= "template_5hfhq15";
+
+	emailjs
+		.send(serviceID, templateID, params)
+		.then((res) =>{
+				document.getElementById("name").value = "";
+				document.getElementById("email").value = "";
+				document.getElementById("message").value = "";
+				
+				console.log(res);
+				alert("your message send successfully");
+			})
+			.catch((err) => console.log(err));
+
+	}
